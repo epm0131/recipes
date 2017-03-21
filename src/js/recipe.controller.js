cb9694889ed25410aa36957130d9bb88fb05b477
Ettore Merendino <merendino.ettore@gmail.com>
2017-03-21 12:38:59 -0400

    created the recipe controller and it is functional

---
+++ src/js/recipe.controller.js
@@ -0,0 +1,10 @@
+(function() {
+  'use strict';
+
+  angular.module('recipe')
+   .controller('RecipeController', RecipeController);
+
+  function RecipeController() {
+    console.log('Create the Recipe Controller');
+  }
+}());
