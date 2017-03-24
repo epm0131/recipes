092d6285f279d8617ec0a39e64fd25c78de91776
Ettore Merendino <merendino.ettore@gmail.com>
2017-03-24 13:21:37 -0400

    added the function that uses the data from the service

--- src/js/recipe.controller.js
+++ src/js/recipe.controller.js
@@ -4,7 +4,24 @@
   angular.module('recipe')
    .controller('RecipeController', RecipeController);
 
-  function RecipeController() {
-    console.log('Create the Recipe Controller');
+  RecipeController.$inject = [ '$q', 'RecipeService'];
+
+  console.log('does the service get created');
+
+  function RecipeController($q, RecipeService) {
+    var vm = this;
+    this.recipe = {};
+
+    this.findRecipe = function findRecipe(ingredient) {
+      RecipeService.getNutritionalData(ingredient)
+      .then(function successHandler(data) {
+        vm.recipe = data;
+        return data;
+      })
+      .catch(function failHandler(xhr) {
+        console.log(xhr);
+      });
+    };
+
   }
 }());
